101e0bfdaee446ef7e02381ca1249cf5f22946f9
92pino <jjb8382@gmail.com>
2022-01-13 00:19:58 +0900

    Fastlane Setting
    - Slack Webhook Add
    - unit test 결과 전달

---
+++ Fastlane-AutoBuild/Fastlane-AutoBuildTests/Fastlane_AutoBuildTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fastlane_AutoBuildTests.swift
+//  Fastlane-AutoBuildTests
+//
+//  Created by 정진배 on 2022/01/12.
+//
+
+import XCTest
+@testable import Fastlane_AutoBuild
+
+class Fastlane_AutoBuildTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+
+    func testExample2() {
+        let a = 1
+        let b = 1
+
+        XCTAssertEqual(a, b)
+    }
+
+}
